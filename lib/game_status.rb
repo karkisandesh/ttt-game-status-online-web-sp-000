67df0cfdf440a5bedca7f0ef65468522981db1f0
Sandesh Karki <karkisandesh@hotmail.com>
2019-01-07 17:26:46 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,6 +3,13 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
+
+   |   |   
+   ------
+   |   |   
+
+
+
 WIN_COMBINATIONS = [
   [0, 1, 2],
   [3, 4, 5],
